9f10b99dd6628edaa98f8127da1eee7ac2b8413e
曹江飞 <coye@MacBook-Pro.local>
2021-08-08 22:53:36 +0800

    first commit

--- app/src/main/java/com/coye/xptest/myServer.java
+++ app/src/main/java/com/coye/xptest/myServer.java
@@ -9,5 +9,9 @@ public class myServer implements IXposedHookLoadPackage{
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         Log.d("xposedHook","=========hook start========");
+        if(!loadPackageParam.packageName.equals("package name")) return;
+        Log.d("xposedHook", String.format("=========find package %s ========", loadPackageParam.packageName));
+        //Todo
+        Log.d("xposedHook","=========hook end========");
     }
 }
