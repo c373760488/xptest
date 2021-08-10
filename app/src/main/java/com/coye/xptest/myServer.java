41d92ad678c5c6d558f52fca4845d62502b5d34e
曹江飞 <coye@MacBook-Pro.local>
2021-08-10 20:58:33 +0800

    first commit

--- app/src/main/java/com/coye/xptest/myServer.java
+++ app/src/main/java/com/coye/xptest/myServer.java
@@ -6,12 +6,20 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class myServer implements IXposedHookLoadPackage{
+    public static int times = 0;
+
+    public static void log(String logInfo){
+        Log.d("xposedHook",logInfo);
+    }
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        Log.d("xposedHook","=========hook start========");
-        if(!loadPackageParam.packageName.equals("package name")) return;
-        Log.d("xposedHook", String.format("=========find package %s ========", loadPackageParam.packageName));
+        Log.d("xposedHook","=====================hook start=====================");
+//        if(!loadPackageParam.packageName.equals("cn.fjzzy.faceapk")) return;
+//        if(times>3) return;
+        times +=1;
+        log(String.format("find package %s ", loadPackageParam.packageName));
         //Todo
-        Log.d("xposedHook","=========hook end========");
+        Log.d("xposedHook","=====================hook end=====================");
+
     }
 }
